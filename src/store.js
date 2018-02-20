a1a433eef5e1637b838997b07f73bb114123cfc0
Gorobey Artem <artemgo@123softawre.ru>
2018-02-20 16:35:32 +0200

    Add react devtools extension

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 
 import reducer from './reducers';
 import mySaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-export default createStore(
-    reducer,
-    applyMiddleware(sagaMiddleware)
-);
+export default createStore( reducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(mySaga);
