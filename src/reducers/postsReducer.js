a5665572735963615a79e0aa050bc6a8aea9ac60
Gorobey Artem <artemgo@123softawre.ru>
2018-03-05 17:41:15 +0200

    Add function get posts

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,9 +1,11 @@
-import { RECEIVE_CREATE_POST } from '../actions/actions';
+import { RECEIVE_GET_POSTS, RECEIVE_CREATE_POST } from '../actions/actions';
 
 const initialState = {posts: {}};
 
 export default (state = initialState, { type, data }) => {
     switch (type) {
+        case RECEIVE_GET_POSTS:
+            return data;
         case RECEIVE_CREATE_POST:
             return {...state, posts: {data, ...state.posts}};
         default:
