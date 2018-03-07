1438af981d513fb891112fcd9c3bfe14393b1a69
Gorobey Artem <artemgo@123softawre.ru>
2018-03-07 16:58:34 +0200

    Fix cookie

--- src/sagas/postsSaga.js
+++ src/sagas/postsSaga.js
@@ -1,13 +1,11 @@
 import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
 import { REQUEST_GET_POSTS, REQUEST_CREATE_POST, receiveGetPosts, receiveCreatePost } from '../actions/actions';
-import Cookies from 'js-cookie';
 import { requestGetPosts, requestCreatePost } from '../api/api';
 
 function* getPostsSaga () {
     try {
         const data = yield call(requestGetPosts);
         if (data && data.headers) {
-            Cookies.set('session', data.headers);
             yield put(receiveGetPosts(data.data.data.posts));
         }
     } catch (e) {
@@ -19,7 +17,6 @@ function* createPostSaga () {
     try {
         const data = yield call(requestCreatePost);
         if (data && data.headers) {
-            Cookies.set('session', data.headers);
             yield put(receiveCreatePost(data.data.post));
         }
     } catch (e) {
