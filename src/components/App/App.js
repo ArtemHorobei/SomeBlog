b28ce7c331d7647de4fb68ba360e46fabac0afea
Gorobey Artem <artemgo@123softawre.ru>
2018-03-27 17:51:17 +0300

    Add scroll up pages after update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,13 @@ import { Switch, Route  } from 'react-router';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
-import PostPage from '../../components/PostPage';
 import MainPage from '../../components/MainPage';
+import PostPage from '../../components/PostPage';
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-const App = () => {
+const App = (props) => {
+    props.history.listen((history) => window.scrollTo(0, 0));
     return (
         <div className="App">
             <Header/>
@@ -20,4 +22,4 @@ const App = () => {
     );
 };
 
-export default App;
+export default withRouter(App);
